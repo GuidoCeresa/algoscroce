55da041c41056bda92ec1085b0aeac18fd96ad65
GuidoCeresa <gac@algos.it>
2015-05-23 06:30:15 +0200

    5.21 fix copy

--- grails-app/taglib/webambulanze/AmbulanzaTagLib.groovy
+++ grails-app/taglib/webambulanze/AmbulanzaTagLib.groovy
@@ -1739,7 +1739,8 @@ class AmbulanzaTagLib {
 //        testo = 'Algos© - 5.17 del 11 febbraio 2015'
 //        testo = 'Algos© - 5.18 del 3 aprile 2015'
 //        testo = 'Algos© - 5.19 del 5 maggio 2015'
-        testo = 'Algos© - 5.20 del 11 maggio 2015'
+//        testo = 'Algos© - 5.20 del 11 maggio 2015'
+        testo = 'Algos© - 5.21 del 23 maggio 2015'
         testo = Lib.tagCella(testo, Aspetto.copyright)
         testoOut = Lib.tagTable(testo)
         return testoOut
